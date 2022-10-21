a68089af49bfc240c3348614915ed42525913ffe
Channchealy Sek <sek.channchealy@yahoo.com>
2022-10-21 15:31:02 -0500

    add User and Post models with testing data at server with backend apollo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,16 @@
+const { User, Post } = require('../models');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,15 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Post {
+        id:ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+
     type Query {
-        sayHi: String!
+        getPosts: [Post]
     }
 `;
 

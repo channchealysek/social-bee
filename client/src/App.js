459ee872f75465c810b2cb161ae2b154b50947cf
Channchealy Sek <sek.channchealy@yahoo.com>
2022-10-22 13:51:25 -0500

    ui and navigate routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import MenuBar from "./components/MenuBar";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1> Social Bee</h1>
-    </div>
+    <BrowserRouter>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 

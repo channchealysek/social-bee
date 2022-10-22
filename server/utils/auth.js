bb2b584c502f4a29a038b260a81af460e61de64f
Channchealy Sek <sek.channchealy@yahoo.com>
2022-10-21 23:00:20 -0500

    impliment functions create post and delete post

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,34 +1,39 @@
-const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require("apollo-server-express");
 
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const jwt = require("jsonwebtoken");
+
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+  authMiddleware: function (context) {
+    /// context = { ... headers }
+    const authHeader = context.req.headers.authorization;
+    if (authHeader) {
+      // Bearer ....
+      const token = authHeader.split("Bearer ")[1];
+      if (token) {
+        try {
+          const user = jwt.verify(token, secret, { maxAge: expiration });
+          return user;
+        } catch (err) {
+          throw new AuthenticationError("Invalid/Expired token");
+        }
+      }
+      throw new Error("Authentication token must be 'Bearer [token]");
     }
 
-    if (!token) {
-      return req;
-    }
-
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-    }
-
-    return req;
+    throw new Error("Authorization header must be provided");
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  signToken: function (user) {
+    return jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      },
+      secret,
+      { expiresIn: "1h" }
+    );
   },
 };

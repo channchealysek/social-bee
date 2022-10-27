ab00781e864509fb55dcf0afaf9793d952e56b07
Channchealy Sek <sek.channchealy@yahoo.com>
2022-10-27 14:41:53 -0500

    edit post

--- client/src/components/EditForm/index.js
+++ client/src/components/EditForm/index.js
@@ -8,16 +8,16 @@ import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_POST } from "../../utils/queries";
 
 export default function PostForm({ postId }) {
-  const { loading, data } = useQuery(QUERY_POST, {
+  const { data } = useQuery(QUERY_POST, {
     variables: { postId },
   });
 
   const getPost = data?.getPost || {};
+  const { body } = getPost;
 
   const { values, onChange, onSubmit } = useForm(editPost, {
     body: "",
   });
-
   console.log(postId);
   console.log(values.body);
 
@@ -30,47 +30,40 @@ export default function PostForm({ postId }) {
     createEditPost();
   }
   let makeUpEditPost;
-  if (loading) {
-  }
-  {
-    const { body } = getPost;
-    values.body = body + "TESTU UPDATE";
-    makeUpEditPost = (
-      <>
-        <Form onSubmit={onSubmit}>
-          <h2>Edit a post:</h2>
-          <Form.Field>
-            <Form.TextArea
-              placeholder="Tell us more"
-              style={{ minHeight: 100 }}
-              name="body"
-              onChange={onChange}
-              value={values.body}
-              error={error ? true : false}
-            />
-            <Button
-              type="submit"
-              color="teal"
-              floated="right"
-              style={{ marginBottom: 20 }}
-            >
-              Update
-            </Button>
-          </Form.Field>
-        </Form>
-        {error && (
-          <div
-            className="ui error message"
-            style={{ marginBottom: 20, width: 350 }}
+  makeUpEditPost = (
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Edit a post:</h2>
+        <Form.Field>
+          <Form.TextArea
+            placeholder="Tell us more"
+            style={{ minHeight: 100 }}
+            name="body"
+            onChange={onChange}
+            value={values.body? body.value : body}
+            error={error ? true : false}
+          />
+          <Button
+            type="submit"
+            color="teal"
+            floated="right"
+            style={{ marginBottom: 20 }}
           >
-            <ul className="list">
-              <li>{error.graphQLErrors[0].message}</li>
-            </ul>
-          </div>
-        )}
-      </>
-    );
-  }
-
+            Update
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div
+          className="ui error message"
+          style={{ marginBottom: 20, width: 350 }}
+        >
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
   return makeUpEditPost;
 }
